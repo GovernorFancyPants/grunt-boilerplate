0aee5f34cb7d7c9e651e160c298bb47638e205b2
GovernorFancyPants <tobiasjohansson123@gmail.com>
2013-12-15 04:59:48 +0100

    First commit grunt-boilerplate

---
+++ gruntfile.js
@@ -0,0 +1,133 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        // sass: {
+        //   dist: {
+        //     options: {
+        //       // cssmin will minify later
+        //       style: 'expanded'
+        //     },
+        //     files: {
+        //       'css/build/global.css': 'css/sass/global.scss'
+        //     }
+        //   }
+        // },
+
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'css/sass',
+                    cssDir: 'css',
+                    require: 'susy',
+                    outputStyle: 'compressed'
+                }
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 version']
+            },
+            multiple_files: {
+                expand: true,
+                flatten: true,
+                src: 'css/*.css',
+                dest: 'css'
+            }
+        },
+
+        cssmin: {
+            combine: {
+                files: {
+                    'css/global.css': ['css/global.css']
+                }
+            }
+        },
+
+        jshint: {
+            beforeconcat: ['js/*.js']
+        },
+
+        concat: {
+            dist: {
+                src: [
+                    'js/vendor/*.js',
+                    'js/main.js'
+                ],
+                dest: 'js/build/production.js'
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'img/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'img/'
+                }]
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            html: {
+                files: ['index.html'],
+                options: {
+                    spawn: false,
+                }
+            },
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify', 'jshint'],
+                options: {
+                    spawn: false,
+                }
+            },
+            css: {
+                files: ['css/sass/*.scss', 'css/sass/partials/*.scss'],
+                tasks: ['compass', 'autoprefixer', 'cssmin'],
+                options: {
+                    spawn: false,
+                }
+            },
+            images: {
+                files: ['img/**/*.{png,jpg,gif}', 'img/*.{png,jpg,gif}'],
+                tasks: ['imagemin'],
+                options: {
+                    spawn: false,
+                }
+            }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: './'
+                }
+            }
+        },
+
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    // Default Task is basically a rebuild
+    grunt.registerTask('default', ['concat', 'uglify', 'compass', 'imagemin']);
+
+    grunt.registerTask('dev', ['connect', 'watch']);
+
+};
